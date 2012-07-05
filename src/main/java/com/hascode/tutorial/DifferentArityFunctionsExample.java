68916cc844b0702cb6ccb5bc4fbb37f24295423c
hascode <hascode@localhost>
2012-07-05 23:03:37 +0200

    functionaljava: examples for functions of different arity added.

---
+++ src/main/java/com/hascode/tutorial/DifferentArityFunctionsExample.java
@@ -0,0 +1,34 @@
+package com.hascode.tutorial;
+
+import static fj.data.Array.array;
+import fj.Effect;
+import fj.F;
+import fj.F2;
+
+public class DifferentArityFunctionsExample {
+	public static void main(final String[] args) {
+		F<String, Boolean> endsWithX = new F<String, Boolean>() {
+			@Override
+			public Boolean f(final String a) {
+				return a.endsWith("x");
+			}
+		};
+
+		F2<String, String, Boolean> stringsEqual = new F2<String, String, Boolean>() {
+			@Override
+			public Boolean f(final String a, final String b) {
+				return a.equals(b);
+			}
+
+		};
+		// output: "barx bazx"
+		array("foo", "barx", "moo", "bazx").filter(endsWithX).foreach(output);
+	}
+
+	static Effect<String> output = new Effect<String>() {
+		@Override
+		public void e(final String a) {
+			System.out.println(a);
+		}
+	};
+}
