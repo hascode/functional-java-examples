86b49967ca8c23facb41b55dc412c781b001904c
hascode <hascode@localhost>
2012-07-13 11:53:00 +0200

    Transformer and closure examples for commons-collections added.

--- src/main/java/com/hascode/tutorial/CommonsCollectionExample.java
+++ src/main/java/com/hascode/tutorial/CommonsCollectionExample.java
@@ -1,11 +1,15 @@
 package com.hascode.tutorial;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
+import org.apache.commons.collections.Closure;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.collections.Predicate;
+import org.apache.commons.collections.Transformer;
 import org.apache.commons.collections.iterators.FilterIterator;
 
 public class CommonsCollectionExample {
@@ -30,5 +34,29 @@ public class CommonsCollectionExample {
 		// output:
 		// Some book
 		// Another book
+
+		Closure closure = new Closure() {
+			public void execute(final Object obj) {
+				Book book = (Book) obj;
+				System.out.println("the book's title is: " + book.getTitle());
+			}
+		};
+		CollectionUtils.forAllDo(books, closure);
+		// the book's title is: Some book
+		// the book's title is: Another book
+		// the book's title is: I luv codin
+
+		Transformer transformer = new Transformer() {
+			public Object transform(final Object obj) {
+				Book book = (Book) obj;
+				return new Book("X " + book.getTitle(), book.getPublished());
+			}
+		};
+		Collection<Book> transformedBooks = CollectionUtils.collect(books,
+				transformer);
+		CollectionUtils.forAllDo(transformedBooks, closure);
+		// the book's title is: X Some book
+		// the book's title is: X Another book
+		// the book's title is: X I luv codin
 	}
 }
