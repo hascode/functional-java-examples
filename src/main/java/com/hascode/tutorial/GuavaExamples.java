b5ddfc34e207916a1045d49c6810595545537c06
hascode <hascode@localhost>
2012-07-13 21:34:05 +0200

    Optional-API examples for Guava added.

--- src/main/java/com/hascode/tutorial/GuavaExamples.java
+++ src/main/java/com/hascode/tutorial/GuavaExamples.java
@@ -4,10 +4,12 @@ import static com.google.common.collect.Lists.newArrayList;
 
 import java.util.List;
 
+import com.google.common.base.Function;
+import com.google.common.base.Optional;
 import com.google.common.base.Predicate;
 import com.google.common.collect.Iterables;
 
-public class GuavaPredicates {
+public class GuavaExamples {
 
 	public static void main(final String[] args) {
 		List<Book> books = newArrayList(new Book("Some book", "Arnold Author",
@@ -16,13 +18,13 @@ public class GuavaPredicates {
 						"Ye ole book", "Ye authorr", 1711), new Book(
 						"Some book - second edition", "Arnold Author", 2010));
 
-		Predicate<Book> newerThanYear2000 = new Predicate<GuavaPredicates.Book>() {
+		Predicate<Book> newerThanYear2000 = new Predicate<Book>() {
 			public boolean apply(final Book book) {
 				return book.getYearPublished() >= 2000;
 			}
 		};
 
-		Predicate<Book> byAuthorArnold = new Predicate<GuavaPredicates.Book>() {
+		Predicate<Book> byAuthorArnold = new Predicate<Book>() {
 			public boolean apply(final Book book) {
 				return book.getAuthor().equals("Arnold Author");
 			}
@@ -34,7 +36,32 @@ public class GuavaPredicates {
 
 		System.out.println("books by author=Arnold Author");
 		System.out.println(Iterables.filter(books, byAuthorArnold).toString());
+
+		Function<Book, String> titleTransformer = new Function<GuavaExamples.Book, String>() {
+			public String apply(final Book book) {
+				return "X " + book.getTitle();
+			}
+		};
+		// outputs:
+		// "[X Some book, X Programmers are weird, X Another book, X Ye ole book, X Some book - second edition]"
+		System.out.println(Iterables.transform(books, titleTransformer));
+
+		Optional<Integer> computed1 = computeIntegerFrom("1234zzx");
+		System.out.println(computed1.isPresent()); // false
+		Optional<Integer> computed2 = computeIntegerFrom("12345");
+		System.out.println(computed2.isPresent()); // true
+		System.out.println(computed2.get()); // 12345
 	}
+
+	private static Optional<Integer> computeIntegerFrom(
+			final String numberAsString) {
+		try {
+			Integer i = Integer.valueOf(numberAsString);
+			return Optional.of(i);
+		} catch (Exception e) {
+		}
+		return Optional.absent();
+	};
 
 	static class Book {
 		final String title;
