f90f5dc4074cf950e61a124c49c24a4314660f7f
hascode <hascode@localhost>
2012-07-14 19:34:26 +0200

    Played around with commons-functor alpha lib.

--- src/main/java/com/hascode/tutorial/CommonsFunctorExample.java
+++ src/main/java/com/hascode/tutorial/CommonsFunctorExample.java
@@ -1,7 +1,21 @@
 package com.hascode.tutorial;
 
+import org.apache.commons.functor.Predicate;
+import org.apache.commons.functor.Procedure;
+import org.apache.commons.functor.core.algorithm.WhileDo;
 
 public class CommonsFunctorExample {
 	public static void main(final String[] args) {
+		Predicate predicate = new Predicate() {
+			public boolean test() {
+				return true;
+			}
+		};
+		Procedure procedure = new Procedure() {
+			public void run() {
+				System.out.println("test");
+			}
+		};
+		new WhileDo(predicate, procedure).run();
 	}
 }
